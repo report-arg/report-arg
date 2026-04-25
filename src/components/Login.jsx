b2974b751d5591df6824af264dcb489aec8f2718
Valentina <valentinapescarav@gmail.com>
2026-04-25 19:45:20 -0300

    Update dependencies and enhance Login component with Heroicons; modify global styles and layout metadata.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,12 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import Logo from '../../public/logo.png';
+import {
+  EnvelopeIcon,
+  EyeIcon,
+  EyeSlashIcon,
+  LockClosedIcon,
+} from '@heroicons/react/20/solid';
 
 export default function Login() {
   const [formData, setFormData] = useState({ email: '', password: '' });
@@ -59,10 +64,7 @@ export default function Login() {
                   required
                 />
                 <div className="form-input-icon">
-                  <svg fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
-                  </svg>
+                  <EnvelopeIcon aria-hidden />
                 </div>
               </div>
             </div>
@@ -71,9 +73,7 @@ export default function Login() {
               <label className="form-label" htmlFor="password">CONTRASEÑA</label>
               <div className="form-input-wrapper">
                 <div className="form-input-icon form-input-icon-left">
-                  <svg fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"/>
-                  </svg>
+                  <LockClosedIcon aria-hidden />
                 </div>
                 <input
                   type={showPassword ? 'text' : 'password'}
@@ -89,18 +89,9 @@ export default function Login() {
                   type="button"
                   className="password-toggle"
                   onClick={() => setShowPassword(!showPassword)}
+                  aria-label={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
                 >
-                  {showPassword ? (
-                    <svg fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clipRule="evenodd"/>
-                      <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"/>
-                    </svg>
-                  ) : (
-                    <svg fill="currentColor" viewBox="0 0 20 20">
-                      <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"/>
-                      <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd"/>
-                    </svg>
-                  )}
+                  {showPassword ? <EyeSlashIcon aria-hidden /> : <EyeIcon aria-hidden />}
                 </button>
               </div>
               <Link href="/reset-password" className="form-link">
